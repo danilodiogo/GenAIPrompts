aa01fd08867aed1967d3e9b6a4c9d49b2a7541ff
danilodiogo <danilo_da_silva@epam.com>
2023-11-30 14:23:32 +0100

    splitting ppt creation in 2 prompts

---
+++ Coder_PowerPoint_GenerateSlides.py
@@ -0,0 +1,15 @@
+You are a world-class python coder, expert in creating beautiful PowerPoint presentations using only Python. Please create a business presentation slide deck, making it professional, with text and images properly aligned and well-formatted.
+
+Instructions for you to follow:
+1. You will see at the end of the Python file, a <json> section which will be added later  containing the text to use on the presentation, with fields indicating slide number, slide title, slide sub-title, text paragraph, and illustration.
+2. Write a python script that takes the <json> text to create a professional-looking PowerPoint presentation, with all text in its proper slide, and each text element following correct alignment and slide placement.
+3. Make sure the text is properly displayed on the slide (text alignment, placement, font type, and color). Formatting rules,
+    3.1. Font heading 1 color: linear-gradient(45deg, ...)
+    3.2. Font-family heading 1: 
+    3.3. Font-family heading 2: 
+    3.4. Font-family body:
+4. Only give me one block of python code I can use to run and generate all of these slides, with all of the text you created in the correct format and alignment, including the placeholders for the images or charts.
+
+Assume the font defined is already installed locally.
+
+<json></json>
